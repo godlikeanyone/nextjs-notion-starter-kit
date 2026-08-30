75dcc28390a6266af68d343e84124535c965f048
Godlike <26649232+godlikeanyone@users.noreply.github.com>
2026-08-30 22:09:50 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '137f8ccb172880bf8722f0f90463cfae',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
